adeb1dff1de36037a528a3dcc882d74c05d5e0a1
Kieran Hegarty <kieranheg@users.noreply.github.com>
2019-06-18 16:52:58 +0100

    Added Object with String test

---
+++ src/main/java/com/muscy/models/Car.java
@@ -0,0 +1,15 @@
+package com.muscy.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class Car {
+    private String make;
+    private String model;
+    private String year;
+    
+}
--- src/test/java/com/muscy/Encryption_UT.java
+++ src/test/java/com/muscy/Encryption_UT.java
@@ -2,21 +2,39 @@ package com.muscy;
 
 import com.muscy.encryption.AesGcmEncryption;
 import com.muscy.encryption.Encryption;
+import com.muscy.models.Car;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class Encryption_UT {
     
-    private static final String ENCRYPTION_KEY = "abcdefabcdefabcdabcdefabcdefabcd";
-    public static final String DATA_TO_ENCRYPT = "test data 01";
+    private static final String ENCRYPTION_KEY_STR = "abcdefabcdefabcdabcdefabcdefabcd";
+    private static final String DATA_TO_ENCRYPT = "test data 01";
+    private static final String ENCRYPTION_KEY_CAR = "abcdefabdefabcdabcdefa0123456789";
     
     private Encryption aes = new AesGcmEncryption();
     
     @Test
-    public void testEncryption() throws Exception {
-        byte[] encryptedData = aes.encrypt(ENCRYPTION_KEY, DATA_TO_ENCRYPT);
-        byte[] decryptedData = aes.decrypt(ENCRYPTION_KEY, encryptedData);
-        assertEquals(DATA_TO_ENCRYPT, Encryption.convertByteArrayToString(decryptedData));
+    public void testEncryptionOfString() {
+        byte[] encryptedData = aes.encrypt(ENCRYPTION_KEY_STR, DATA_TO_ENCRYPT);
+        assertEquals(DATA_TO_ENCRYPT, getDecryptedString(ENCRYPTION_KEY_STR, encryptedData));
+    }
+    
+    @Test
+    public void testEncryptionOfCarObject() {
+        Car car = Car.builder().make("Honda").model("Civic").year("2005").build();
+    
+        byte[] encryptedMake = aes.encrypt(ENCRYPTION_KEY_CAR, car.getMake());
+        byte[] encryptedModel = aes.encrypt(ENCRYPTION_KEY_CAR, car.getModel());
+        byte[] encryptedYear = aes.encrypt(ENCRYPTION_KEY_CAR, car.getYear());
+    
+        assertEquals(car.getMake(), getDecryptedString(ENCRYPTION_KEY_CAR, encryptedMake));
+        assertEquals(car.getModel(), getDecryptedString(ENCRYPTION_KEY_CAR, encryptedModel));
+        assertEquals(car.getYear(), getDecryptedString(ENCRYPTION_KEY_CAR, encryptedYear));
+    }
+    
+    private String getDecryptedString(final String key, final byte[] encryptedData) {
+        return Encryption.convertByteArrayToString(aes.decrypt(key, encryptedData));
     }
 }
